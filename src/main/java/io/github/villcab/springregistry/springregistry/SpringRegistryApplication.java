4041d60f334d18dc6efc1ae8d21b935ee84e5335
Bismarck Villca Soliz <bvillca@vulcan.bo>
2018-06-02 11:02:24 -0400

    Spring Registry Base

---
+++ src/main/java/io/github/villcab/springregistry/springregistry/SpringRegistryApplication.java
@@ -0,0 +1,12 @@
+package io.github.villcab.springregistry.springregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRegistryApplication.class, args);
+	}
+}
